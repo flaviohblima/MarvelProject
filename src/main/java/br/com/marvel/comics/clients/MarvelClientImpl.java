f5ccf3c575c7bcc40de5ee49540d8d51d486e262
flavio.hblima <flavio.hblima@gmail.com>
2024-08-10 10:48:46 -0300

    refactor: clean Marvel client (make methods testable and test them).

--- src/main/java/br/com/marvel/comics/clients/MarvelClientImpl.java
+++ src/main/java/br/com/marvel/comics/clients/MarvelClientImpl.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.client.RestTemplate;
@@ -20,35 +19,35 @@ public class MarvelClientImpl implements MarvelClient {
     private final String publicKey;
     private final String privateKey;
     private final RestTemplate restTemplate;
-    private String marvelGateway = "http://gateway.marvel.com/v1/public";
 
     public MarvelClientImpl(@Value("${comics.public-key}") String publicKey,
-                            @Value("${comics.private-key}") String privateKey) {
+                            @Value("${comics.private-key}") String privateKey,
+                            RestTemplate restTemplate) {
         this.publicKey = publicKey;
         this.privateKey = privateKey;
-        this.restTemplate = new RestTemplate();
+        this.restTemplate = restTemplate;
     }
 
     public ComicDataWrapper listComics() {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        String ts = formatter.format(new Date());
-        String hash = createEncryptedHash(ts);
-
-        // /comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
-        String endpoint = marvelGateway +
-                "/comics" +
-                "?ts=" + ts +
-                "&apikey=" + publicKey +
-                "&hash=" + hash;
-
-        ResponseEntity<ComicDataWrapper> response = restTemplate.exchange(
-                endpoint,
+        String url = buildGetComicsUrl();
+        return restTemplate.exchange(
+                url,
                 HttpMethod.GET,
                 HttpEntity.EMPTY,
                 ComicDataWrapper.class
-        );
+        ).getBody();
+    }
 
-        return response.getBody();
+    protected String buildGetComicsUrl() {
+        String ts = formatTimestamp(new Date());
+        String hash = createEncryptedHash(ts);
+        String marvelGateway = "http://gateway.marvel.com/v1/public";
+        return marvelGateway + "/comics?ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash;
+    }
+
+    protected String formatTimestamp(Date seedDate) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        return formatter.format(seedDate);
     }
 
     protected String createEncryptedHash(String ts) {
--- src/test/java/br/com/marvel/comics/clients/MarvelClientTest.java
+++ src/test/java/br/com/marvel/comics/clients/MarvelClientTest.java
@@ -1,11 +1,23 @@
 package br.com.marvel.comics.clients;
 
-import org.junit.jupiter.api.Assertions;
+import br.com.marvel.comics.clients.dto.comics.ComicDataWrapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MarvelClientTest {
@@ -14,20 +26,52 @@ class MarvelClientTest {
     private RestTemplate restTemplate;
 
     @Test
+    void listComics() {
+        MarvelClientImpl client = spy(new MarvelClientImpl("publicKey", "privateKey", restTemplate));
+        when(client.buildGetComicsUrl()).thenReturn("mockedUrl");
+
+        ComicDataWrapper expected = mock(ComicDataWrapper.class);
+        ResponseEntity<ComicDataWrapper> expectedResponse = new ResponseEntity<>(expected, HttpStatus.OK);
+        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), eq(HttpEntity.EMPTY), eq(ComicDataWrapper.class)))
+                .thenReturn(expectedResponse);
+
+        ComicDataWrapper actual = client.listComics();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void buildGetComicsUrl() {
+        MarvelClientImpl client = spy(new MarvelClientImpl("publicKey", "privateKey", restTemplate));
+        doReturn("timestamp").when(client).formatTimestamp(any(Date.class));
+        when(client.createEncryptedHash("timestamp")).thenReturn("someHash");
+
+        String expected = "http://gateway.marvel.com/v1/public/comics?ts=timestamp&apikey=publicKey&hash=someHash";
+        String actual = client.buildGetComicsUrl();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void createTimestamp() throws ParseException {
+        MarvelClientImpl client = spy(new MarvelClientImpl("publicKey", "privateKey", restTemplate));
+
+        String expected = "2000-01-02_01-02-03";
+        Date parsedDate = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
+                .parse(expected);
+
+        String actual = client.formatTimestamp(parsedDate);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
     void createEncryptedHash() {
-        // arrange > objetos
-        String expected = "ffd275c5130566a2916217b101f26150";
-        String publicKey = "1234";
-        String privateKey = "abcd";
+        MarvelClientImpl client = new MarvelClientImpl("1234", "abcd", restTemplate);
         String ts = "1";
-
-        MarvelClientImpl client = new MarvelClientImpl(publicKey, privateKey);
-
-        // act > chama o método
         String actual = client.createEncryptedHash(ts);
-
-        // assert > assegura que deu certo
-        Assertions.assertEquals(expected, actual);
+        assertEquals("ffd275c5130566a2916217b101f26150", actual);
     }
 
 }
